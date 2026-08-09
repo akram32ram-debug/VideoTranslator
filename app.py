85bb49550b5c1ed4b838eec7dd7ae5f7aeb86f48
akram32ram-debug <akram32ram@gmail.com>
2026-08-09 15:51:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-st.markdown('<meta name="google-site-verification" content="YAHAN_APNA_NAYA_CODE_PASTE_KAREIN" />', unsafe_allow_html=True)
+st.markdown('<meta name="google-site-verification" content="O8FzJ2HI4_XeHf3nD061ujpOB2aWw3kpaTI6BSCh3PA" />', unsafe_allow_html=True)
 
 # --- Custom Styling (SaaS Look) ---
 st.markdown("""
